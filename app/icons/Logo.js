9013b4db9a90e001377412ba462a1f69d5da44df
yabeshan <beshan@bigmir.net>
2020-10-18 16:25:25 +0300

    import svg

--- app/icons/Logo.js
+++ app/icons/Logo.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
+
 const Component = () => (
   <View>
-    <Text>Cleaned Logo from index</Text>
+    <Text>Import Svg</Text>
+    <Svg></Svg>
   </View>
 )
 
