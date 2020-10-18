d6b9b90e8b288f58983cc57a2bf57acfd62409e5
yabeshan <beshan@bigmir.net>
2020-10-18 16:43:34 +0300

    real svg

--- app/icons/Logo.js
+++ app/icons/Logo.js
@@ -26,10 +26,25 @@ import Svg, {
 
 const Component = () => (
   <View>
-    <Text>Import Svg</Text>
+    <Text>Circle Rect</Text>
     <Svg height="50%" width="50%" viewBox="0 0 100 100">
-      <Circle />
-      <Rect />
+      <Circle
+        cx="50"
+        cy="50"
+        r="45"
+        stroke="blue"
+        strokeWidth="2.5"
+        fill="green"
+      />
+      <Rect
+        x="15"
+        y="15"
+        width="70"
+        height="70"
+        stroke="red"
+        strokeWidth="2"
+        fill="yellow"
+      />
     </Svg>
   </View>
 )
